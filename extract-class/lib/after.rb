cf4a4781cbbec24ebe5422a3063f0daca1e01ef5
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-10-14 10:38:20 +0300

    Do Extract Class exercise

--- extract-class/lib/after.rb
+++ extract-class/lib/after.rb
@@ -1,54 +1,44 @@
-class Student
+class Term
+  attr_accessor :assiduity, :tet, :behavior
 
+  def grade
+    (@assiduity + @test + @behavior) / 3
+  end
+
+  def set_all_to(value)
+    @assiduity, @test, @behavior = value, value, value
+  end
+end
+
+
+class Student
   attr_reader :terms
 
   def initialize
     @terms = [
-      Term.new(:first),
-      Term.new(:second),
-      Term.new(:third)
+      Term.new,
+      Term.new,
+      Term.new
     ]
   end
 
   def set_all_grades_to grade
-    terms.each { |term| term.set_all_grades_to(grade) }
+    @terms.each { |term| term.set_all_to(grade) }
+  end
+
+  def grade_for_term(index)
+    @terms[index].grade
   end
 
   def first_term_grade
-    term(:first).grade
+    terms[0].grade
   end
 
   def second_term_grade
-    term(:second).grade
+    terms[1].grade
   end
 
   def third_term_grade
-    term(:third).grade
-  end
-
-  def term reference
-    terms.find { |term| term.name == reference }
-  end
-end
-
-class Term
-
-  attr_reader :name, :assiduity, :test, :behavior
-
-  def initialize name
-    @name      = name
-    @assiduity = 0
-    @test      = 0
-    @behavior  = 0
-  end
-
-  def set_all_grades_to grade
-    @assiduity = grade
-    @test      = grade
-    @behavior  = grade
-  end
-
-  def grade
-    (assiduity + test + behavior) / 3
+    terms[2].grade
   end
 end
